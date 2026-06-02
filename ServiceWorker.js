b90210f2564b75000c7d722cd860c2c99b382b87
PauloRade <paulomendozaradenta@gmail.com>
2026-06-02 21:52:10 +0800

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-bacolod-project-frontend-0.0.3";
 const contentToCache = [
-    "Build/bacolod-web-build.loader.js",
-    "Build/bacolod-web-build.framework.js.unityweb",
-    "Build/bacolod-web-build.data.unityweb",
-    "Build/bacolod-web-build.wasm.unityweb",
+    "Build/8e8193bb04ba07e4649b40eb1e030f2e.loader.js",
+    "Build/f81453513d71ae9bc034e59202c475fc.framework.js.unityweb",
+    "Build/5d5a24e208d57e7f951f9a70740f77d1.data.unityweb",
+    "Build/3f8f05e7ca8229f007929cac75c05ad7.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
